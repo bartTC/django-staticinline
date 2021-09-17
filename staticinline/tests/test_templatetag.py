487a1f3faa75a4fc4bd9a0e8567bcd5c2f19726e
Martin Mahner <martin@mahner.org>
2021-09-17 12:41:00 +0200

    Testsuite updates

--- staticinline/tests/test_templatetag.py
+++ staticinline/tests/test_templatetag.py
@@ -7,10 +7,7 @@ from django.test.testcases import TestCase
 
 from staticinline.templatetags import staticinline
 
-try:
-    from unittest import mock
-except ImportError:
-    import mock
+from unittest import mock
 
 
 def render(source, **context):
@@ -19,15 +16,14 @@ def render(source, **context):
 
 class StaticInlineTests(TestCase):
     template = (
-        '{% load staticinline %}'
-        '<script>{% staticinline "somefile" %}</script>'
+        "{% load staticinline %}" '<script>{% staticinline "somefile" %}</script>'
     )
 
     def test_found(self):
         """
         Static file is are correctly included inline the template.
         """
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
+        with mock.patch.object(staticinline, "read_static_file") as reader:
             reader.return_value = 'alert("hi")'
             rendered = render(self.template)
             self.assertEqual(rendered, '<script>alert("hi")</script>')
@@ -37,10 +33,10 @@ class StaticInlineTests(TestCase):
         An empty string is returned if the inlined file is missing and
         DEBUG is off.
         """
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
-            reader.side_effect = ValueError('Not found')
+        with mock.patch.object(staticinline, "read_static_file") as reader:
+            reader.side_effect = ValueError("Not found")
             rendered = render(self.template)
-            self.assertEqual(rendered, '<script></script>')
+            self.assertEqual(rendered, "<script></script>")
 
     @override_settings(DEBUG=True)
     def test_file_missing_debug(self):
@@ -48,18 +44,18 @@ class StaticInlineTests(TestCase):
         An error raised from read_static_file if the inlined file is
         missing and DEBUG is on.
         """
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
-            reader.side_effect = ValueError('Not found')
+        with mock.patch.object(staticinline, "read_static_file") as reader:
+            reader.side_effect = ValueError("Not found")
             self.assertRaises(ValueError, render, self.template)
 
 
 class CachedStaticInlineTests(TestCase):
     template_cached = (
-        '{% load staticinline %}'
+        "{% load staticinline %}"
         '<script>{% staticinline "somefile" cache=True cache_timeout=60 %}</script>'
     )
     template_cached_encoder = (
-        '{% load staticinline %}'
+        "{% load staticinline %}"
         '{% staticinline "some-other-file" encode="base64" cache=True cache_timeout=60 %}'
     )
 
@@ -71,13 +67,13 @@ class CachedStaticInlineTests(TestCase):
         Static file is are correctly included inline the template.
         """
         # Call once to set in cache
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
+        with mock.patch.object(staticinline, "read_static_file") as reader:
             reader.return_value = 'alert("hi")'
             rendered = render(self.template_cached)
             self.assertEqual(rendered, '<script>alert("hi")</script>')
 
         # Call again to test cache retrieval
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
+        with mock.patch.object(staticinline, "read_static_file") as reader:
             reader.return_value = 'alert("hi")'
             rendered = render(self.template_cached)
             self.assertEqual(rendered, '<script>alert("hi")</script>')
@@ -87,16 +83,16 @@ class CachedStaticInlineTests(TestCase):
         Static file is are correctly included inline the template.
         """
         # Call once to set in cache
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
-            reader.return_value = b'it is bytestring data'
+        with mock.patch.object(staticinline, "read_static_file") as reader:
+            reader.return_value = b"it is bytestring data"
             rendered = render(self.template_cached_encoder)
-            self.assertEqual(rendered, 'aXQgaXMgYnl0ZXN0cmluZyBkYXRh')
+            self.assertEqual(rendered, "aXQgaXMgYnl0ZXN0cmluZyBkYXRh")
 
         # Call again to test cache retrieval
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
-            reader.return_value = b'it is bytestring data'
+        with mock.patch.object(staticinline, "read_static_file") as reader:
+            reader.return_value = b"it is bytestring data"
             rendered = render(self.template_cached_encoder)
-            self.assertEqual(rendered, 'aXQgaXMgYnl0ZXN0cmluZyBkYXRh')
+            self.assertEqual(rendered, "aXQgaXMgYnl0ZXN0cmluZyBkYXRh")
 
 
 class EncoderTests(TestCase):
@@ -105,10 +101,10 @@ class EncoderTests(TestCase):
         Using an unregistered encoder will raise ImproperlyConfigured.
         """
         template = (
-            '{% load staticinline %}'
+            "{% load staticinline %}"
             '{% staticinline "somefile" encode="doesnotexist" %}'
         )
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
+        with mock.patch.object(staticinline, "read_static_file") as reader:
             reader.return_value = 'alert("hi")'
             self.assertRaises(ImproperlyConfigured, render, template)
 
@@ -117,13 +113,13 @@ class EncoderTests(TestCase):
         If the encoder raises any exception, return an empty string when
         DEBUG is off.
         """
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
-            reader.return_value = b'it is bytestring data'
+        with mock.patch.object(staticinline, "read_static_file") as reader:
+            reader.return_value = b"it is bytestring data"
             rendered = render(
-                '{% load staticinline %}'
+                "{% load staticinline %}"
                 'My Key: {% staticinline "somefile" encode="broken" %}'
             )
-            self.assertEqual(rendered, 'My Key: ')
+            self.assertEqual(rendered, "My Key: ")
 
     @override_settings(DEBUG=True)
     def test_error_debug(self):
@@ -131,10 +127,10 @@ class EncoderTests(TestCase):
         If the encoder raises any exception, the exception is raised
         when DEBUG is on.
         """
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
-            reader.return_value = b'it is bytestring data'
+        with mock.patch.object(staticinline, "read_static_file") as reader:
+            reader.return_value = b"it is bytestring data"
             template = (
-                '{% load staticinline %}'
+                "{% load staticinline %}"
                 'My Key: {% staticinline "somefile" encode="broken" %}'
             )
             self.assertRaises(ZeroDivisionError, render, template)
@@ -144,35 +140,35 @@ class EncoderTests(TestCase):
         Custom encoders can be used in a separate appconfig.
         In testapp/apps.py, the 'uppercase' encoder is added.
         """
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
-            reader.return_value = b'shouting'
+        with mock.patch.object(staticinline, "read_static_file") as reader:
+            reader.return_value = b"shouting"
             rendered = render(
-                '{% load staticinline %}'
+                "{% load staticinline %}"
                 '{% staticinline "somefile" encode="uppercase" %}'
             )
-            self.assertEqual(rendered, 'SHOUTING')
+            self.assertEqual(rendered, "SHOUTING")
 
     def test_base64(self):
         """
         The 'base64' encoder is shipped with this application.
         """
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
-            reader.return_value = b'it is bytestring data'
+        with mock.patch.object(staticinline, "read_static_file") as reader:
+            reader.return_value = b"it is bytestring data"
             rendered = render(
-                '{% load staticinline %}'
+                "{% load staticinline %}"
                 'My Key: {% staticinline "somefile" encode="base64" %}'
             )
-            self.assertEqual(rendered, 'My Key: aXQgaXMgYnl0ZXN0cmluZyBkYXRh')
+            self.assertEqual(rendered, "My Key: aXQgaXMgYnl0ZXN0cmluZyBkYXRh")
 
     @override_settings(DEBUG=True)
     def test_data(self):
         """
         The 'data' URI encoder is shipped with this application.
         """
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
-            reader.return_value = b'png content'
+        with mock.patch.object(staticinline, "read_static_file") as reader:
+            reader.return_value = b"png content"
             rendered = render(
-                '{% load staticinline %}'
+                "{% load staticinline %}"
                 '<img src="{% staticinline "a.png" encode="data" %}">'
             )
             self.assertEqual(
@@ -184,24 +180,22 @@ class EncoderTests(TestCase):
         """
         The data URI still works if no mimetype can be guessed.
         """
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
-            reader.return_value = b'png content'
+        with mock.patch.object(staticinline, "read_static_file") as reader:
+            reader.return_value = b"png content"
             rendered = render(
-                '{% load staticinline %}'
+                "{% load staticinline %}"
                 '<img src="{% staticinline "somefile" encode="data" %}">'
             )
-            self.assertEqual(
-                rendered, '<img src="data:;base64,cG5nIGNvbnRlbnQ=">'
-            )
+            self.assertEqual(rendered, '<img src="data:;base64,cG5nIGNvbnRlbnQ=">')
 
     def test_sri(self):
         """
         The 'base64' encoder is shipped with this application.
         """
-        with mock.patch.object(staticinline, 'read_static_file') as reader:
+        with mock.patch.object(staticinline, "read_static_file") as reader:
             reader.return_value = b'alert("hi")'
             rendered = render(
-                '{% load staticinline %}'
+                "{% load staticinline %}"
                 'integrity="{% staticinline "somefile" encode="sri" %}"'
             )
             self.assertEqual(
